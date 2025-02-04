b8cbe284bb7d4ebb00c6c9c4a966e205ac18641e
Devine-Art <adevine149@gmail.com>
2025-02-04 17:18:08 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Dominion_of_Kings
+{
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        //ask user for variables and assign them
+        Console.WriteLine("Enter # of estates");
+        int estate = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter # of duchy's");
+        int duchy = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter # of provinces");
+        int province = int.Parse(Console.ReadLine());
+
+
+        //Calculate point totals
+        int points = estate + duchy + province
+
+        //write results to console
+        Console.WriteLine("# of points");
+        Console.WriteLine(points);
+
+
+    }
+}
+}
